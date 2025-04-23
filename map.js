409c15f3693a28364f5a02c2d85c48fddb682dcd
priyanshi012 <priyanshisaini1703@gmail.com>
2025-04-23 16:15:44 +0530

    map

---
+++ map.js
@@ -0,0 +1,67 @@
+//  //create a map
+//  const fruits = new Map([
+//     ["apple",500],
+//     ["bananana",300],
+//     ["orange",200]
+// ]);
+// console.log(fruits);
+
+//create a map
+// const fruits = new Map();
+// fruits.set("apple",500),
+// fruits.set  ("bananana",300),
+// fruits.set ("orange",200)
+
+// console.log(fruits)
+// console.log(fruits.size)
+
+// let text ="";
+// fruits.forEach(function(value,key){
+//     text += key+'='+value;
+// })
+// console.log(text);
+
+//map.entity
+// let text ="";
+// for(const x of fruits.entries()){
+//     text += x;
+// }
+// console.log(text);
+// let text =" ";
+// for(const x of fruits.keys()){
+//     text += x;
+// }
+// console.log(text);
+
+// let total =0;
+// for(const x of fruits.values()){
+//     total += x;
+// }
+// console.log(total);
+
+//objects
+// const apples ={name:'Apple'};
+// const banana ={name:'Banana'};
+// const orange ={name:'Orange'};
+// const fruits =new Map();
+
+// fruits.set(apples,500);
+// fruits.set(banana,300);
+// fruits.set(orange,200);
+
+// console.log(fruits.get("apple")); //undefined
+
+//destructuring //no change in object
+// const person={
+//     firstName :"john",
+//     lastName :"doe",
+//     age : 50
+// };
+// let{firstName,lastName}=person;
+
+// console.log(firstName);
+// console.log(lastName); //order of the property does not matters
+ 
+//alias creation to chnage the name of key values
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,9 +1,5 @@
-// function addTwoNumbers(x,y){
-//            console.log(x+y);
-    
-//     }
-//     let arr =[5,9];
-//     addTwoNumbers(...arr);
-//     function addTwoNumbers(x,y,z,a){
-//         console.log(x+y+z+a);
-//     }
+let text ="";
+fruits.forEach(function(value,key){
+    text += key+'='+value;
+})
+console.log(text);
