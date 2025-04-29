1b11a152e07ee43b2288b345c305b4ee44af7e66
priyanshi012 <priyanshisaini1703@gmail.com>
2025-04-29 12:56:56 +0530

    day8

--- map.js
+++ map.js
@@ -63,5 +63,69 @@
 // console.log(lastName); //order of the property does not matters
  
 //alias creation to chnage the name of key values
+// let name ="GurukulTHeSchool"
+// let[a1,a2,a3,a4,a5] = name;
+// console.log(name)
 
+// const fruits =["Banana","orange","apple","mango"];
+// let[fruits1,fruits2]=fruits;
+// console.log(fruits1);
+// console.log(fruits2);
+
+// const fruits =["Banana","orange","apple","mango"];
+// let[fruits1,,,fruits2]=fruits;
+// console.log(fruits1);
+// console.log(fruits2);
+// const fruits =["Banana","orange","apple","mango"];
+// let{[0]:fruits1,[1]:fruits2}=fruits;
+// console.log(fruits1)
+
+//rest property
+// const numbers =[10,20,30,40,50,60]
+// const[a,b,...rest]=numbers;
+// console.log(a)
+// console.log(b);
+// console.log(...rest)
+
+// let firstName = "priyanshi";
+// let lastName = "saini";
+
+// [firstName,lastName]=[lastName ,firstName];
+// console.log(firstName);
+// console.log(lastName)
+
+//exponentiation
+// let x =5;
+// let z = x**2;
+// console.log(z);
+// let x =5;
+// let z = Math.pow(x,2);
+// console.log(z)
+
+// let x = 5;
+// x **= 2;
+// console.log(x);
+
+//array includes()
+//  const fruits =["Banana","orange","apple","mango"];
+//  console.log(fruits.includes("mango"));
+
+// const fruits =["Banana","orange","apple","mango"];
+// console.log(fruits.includes("banana",3));
+
+//tralling commas in array
+// const arr=[
+//     "one",
+//     "two",
+//     "three",,
+// ];
+// const arr=[,];
+
+// const person ={
+//     firstName :"john",
+//     lastName:"dewis",,
+    
+//     age :30,
+// }
+// console.log(person)
 
